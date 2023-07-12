94dfb836d101b99b07811da9458ef35c99f7b2e3
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-07-12 17:08:53 +0300

    feat: react generics and usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+import { Counter } from "./components/Counter";
+import { useState } from "react";
+import List from "./components/List";
+
 function App() {
-  return <h1>Hello</h1>;
+  const [count, setCount] = useState<number>(1);
+
+  return (
+    <>
+      <Heading title="Hello" />
+      <Section title="Different Title">This is my section.</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["Coffee", "Tacos", "Code"]}
+        render={(item: string) => <span className="bold">{item}</span>}
+      ></List>
+    </>
+  );
 }
 
 export default App;
